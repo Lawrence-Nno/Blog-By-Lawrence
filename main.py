27fb2693a52645d35eec2e987442a33e07374fa5
Lawrence Nno <lawrence.nno@gmail.com>
2024-02-06 09:52:16 +0100

    Added PostGres DB

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ login_manager = LoginManager()
 app = Flask(__name__)
 Bootstrap(app)
 ckeditor = CKEditor(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+# Connect to DB
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///posts.db")
 app.secret_key = os.environ["key"]
 db.init_app(app)
 login_manager.init_app(app)
